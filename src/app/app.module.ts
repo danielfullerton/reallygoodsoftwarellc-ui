d8a09d8d833016bd685580d4b7eb018980757441
Daniel <dan.frank.fullerton@gmail.com>
2020-03-20 16:32:17 -0400

    Hero done w/o menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero/hero/hero.component';
+import { MenuComponent } from './hero/menu/menu.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    MenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
